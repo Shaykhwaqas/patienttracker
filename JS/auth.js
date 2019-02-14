ffb15c4701f0332c7be1d3b4a7bd12a123d8cec8
shaykhwaqas <shaykhwaqas1@gmail.com>
2019-02-14 21:43:58 +0500

    FUUAST Project.

---
+++ JS/auth.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAXsfAhy4kyWK-4k7CUptdEtOo-ltnYWh8",
+  authDomain: "patient-tracker-e7044.firebaseapp.com",
+  databaseURL: "https://patient-tracker-e7044.firebaseio.com",
+  projectId: "patient-tracker-e7044",
+  storageBucket: "patient-tracker-e7044.appspot.com",
+  messagingSenderId: "981535073530"
+};
+firebase.initializeApp(config);
